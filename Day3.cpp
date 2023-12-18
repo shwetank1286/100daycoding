fcf5a20d655e7d92354e897d2dc9bd5a9d7152b3
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-18 22:33:03 +0530

    Add files via upload

---
+++ Day3.cpp
@@ -0,0 +1,141 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+    
+//break and continue in for loop 
+    for(int i=0;i<40;i++)
+    {
+        
+        if (i==2){
+            continue;
+        }
+        else if(i==5){
+            break;
+        }cout<<i<<endl;
+    }
+
+//break and continue in while loop
+
+    int i=0;
+    while(i<40){
+        
+        i++;
+        if (i==2){
+            continue;
+        }
+        else if (i==7){
+            continue;
+        }
+        else if(i==9){
+            break;
+        }cout<<i<<endl;
+    }
+    
+//break and continue in do-while loop
+    int i=0;
+    do{
+        
+        i++;
+        if (i==2){
+            continue;
+        }
+        else if (i==7){
+            continue;
+        }
+        else if(i==9){
+            break;
+        }cout<<i<<endl;
+    }while(i<40);
+    
+//pointers
+    int a=3;
+    int *b=&a;
+    cout<<&a<<endl;
+    cout<<b<<endl;
+    cout<<*b<<endl;
+//pointers to pointers
+    int **c=&b;
+    cout<<&b<<endl;
+    cout<<c<<endl;
+    cout<<*c<<endl;
+    cout<<**c<<endl;
+    
+//array
+
+    int marks[]={32,24,25,26};
+    
+    int mathsmarks[4];
+    
+    mathsmarks[0]=199;
+    mathsmarks[1]=189;
+    mathsmarks[2]=179;
+    mathsmarks[3]=198;
+    
+    cout<<"these are maths marks"<<endl;
+    cout<<mathsmarks[0]<<endl;
+    cout<<mathsmarks[1]<<endl;
+    cout<<mathsmarks[2]<<endl;
+    cout<<mathsmarks[3]<<endl;
+    
+    cout<<"these are marks"<<endl;
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    
+    marks[2]=98;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    
+//array using for loop 
+    
+    int marks[]={32,24,25,26};
+    marks[2]=27;
+    for (int i = 0; i < 4; i++) {
+        cout<<"the value of marks "<<i<<" is "<<marks[i]<<endl;
+    }
+    
+//array using for loop 
+
+    int marks[]={32,24,25,26};
+    marks[2]=27;
+    int i=0;
+    while(i<4){
+        cout<<"the value of marks "<<i<<" is "<<marks[i]<<endl;
+        i++;
+    }
+    
+//array using for loop 
+    int marks[]={32,24,25,26};
+    marks[2]=27;
+    int i=0;
+    do{
+        cout<<"the value of marks "<<i<<" is "<<marks[i]<<endl;
+        i++;
+    }while(i<4);
+    
+//array using pointers
+    int marks[]={32,24,25,26};
+    int *p=marks;
+    cout<<"the value of *(p) is "<<*p<<endl;
+    cout<<"the value of *(p+1) is "<<*(p+1)<<endl;
+    cout<<"the value of *(p+2) is "<<*(p+2)<<endl;
+    cout<<"the value of *(p+3) is "<<*(p+3)<<endl;
+    
+//array usingf pointes 
+    int marks[]={32,24,25,26};
+    int *p=marks;
+    cout<<*(p++)<<endl;
+    cout<<*(++p)<<endl;
+    */
+
+    return 0;
+}
