2490eb54e557994d2ad29820527614be9d343e81
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-18 22:38:53 +0530

    Add files via upload

---
+++ Day2.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+//constant 
+    const int a = 5;
+    cout << "the value of a was:" << a << endl;
+    a = 55;
+    cout << "the value of a is:" << a << endl;
+    
+//minipulator
+    
+    int a=1, b=23, c=4321;
+    cout << "the value of a without setw was: " << a << endl;
+    cout << "the value of a without setw was: " << b << endl;
+    cout << "the value of a without setw was: " << c << endl;
+    cout << "the value of a with setw was: " << setw(4)<< a << endl;
+    cout << "the value of a with setw was: " << setw(4)<< b << endl;
+    cout << "the value of a with setw was: " << setw(4)<< c << endl;    
+    
+// if else statement
+    
+    int age;
+    cout << "Tell me your age: "<<endl;
+    cin >> age;
+    if (age<18){
+        cout << "your are a kid you can't come to the party"<<endl;
+    }
+    else if (age=18){
+        cout << "you can come to the party with the help of the kids pass"<<endl;
+    }
+    else{
+        cout << "you can come to the party"<<endl;
+    }
+    
+//switch case statement
+    
+    int age;
+    cout << "Tell me your age: "<<endl;
+    cin >> age;
+    switch (age)
+    {
+        case 18:
+        cout<<"you are 18"<<endl;
+        break;
+        case 44:
+        cout<<"you are 44"<<endl;
+        break;
+        case 2:
+        cout<<"you are 2"<<endl;
+        break;
+        default:
+        cout<<"no special test case"<<endl;
+        break;
+    }
+    cout<<"Done with all the test cases";
+    
+//loop for loop
+    for(int i=0; i<=40;i++){
+        cout<<i<<endl;
+    }
+// infininte for loop
+    for(int i=0; 34<=40;i++){
+        cout<<i<<endl;
+    }
+    
+//loop while loop
+    int i=1;
+    while(i<40){
+        cout<<i<<endl;
+        i++;
+    }
+// infininte for loop
+    int i=1;
+    while(true){
+        cout<<i<<endl;
+        i++;
+    }
+// do while loop
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<40);
+//we use do while because is always do print the first value when the code is false after that also
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(false);
+    
+//---------------------------------------------------------------------*---------------------------------------------------------------------
+// print the table of 5 with the help of the for loop 
+
+    
+    int a=5;
+    for(int i=1; i<=10;i++){
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+    }
+//print the table of any number from the user with the help of for loop
+    int a=5;
+    cout<<"enter the value of a: "<<endl;
+    cin>>a;
+    for(int i=1; i<=10;i++){
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+    }
+// print the table of 5 with the help of the while loop 
+    int a=5,i=1;
+    while(i<=10){
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+        i++;
+    }
+//print the table of any number from the user with the help of while loop
+    int a,i=1;
+    cout<<"enter the value of a: "<<endl;
+    cin>>a;
+    while(i<=10){
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+        i++;
+    }
+// print the table of 5 with the help of the do-while loop 
+    int a=5,i=1;
+    do{
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+        i++;
+    }while(i<=10);
+//print the table of any number from the user with the help of do-while loop
+    int a,i=1;
+    cout<<"enter the value of a: "<<endl;
+    cin>>a;
+    do{
+        cout<<i<<" X "<<a<<" = "<<i*a<<" "<<endl;
+        i++;
+    }while(i<=10);*/
+    return 0;
+}
