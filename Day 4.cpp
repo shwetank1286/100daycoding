5d1e88d585b06a3a1b27dd90fccb0f502243c885
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-19 23:28:10 +0530

    Add files via upload

---
+++ Day 4.cpp
@@ -0,0 +1,181 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+        /*
+int main(){
+    
+    cout<<"JAY MAHAKAL";
+    
+    
+//structures 
+    struct employee
+    {
+        int eId;
+        char favChar;
+        float salary;
+    };
+    int main(){
+        struct employee shwetank;
+        struct employee shweta;
+        struct employee beauty;
+        cout<<"the value in shwetank"<<endl;
+        shwetank.eId=1;
+        shwetank.favChar = 'c';
+        shwetank.salary=120000;
+        cout<<"the value is "<<shwetank.eId<<endl;
+        cout<<"the value is "<<shwetank.favChar<<endl;
+        cout<<"the value is "<<shwetank.salary<<endl<<endl;
+        
+        cout<<"the value in shweta"<<endl;
+        shweta.eId=2;
+        shweta.favChar = 's';
+        shweta.salary=19000;
+        cout<<"the value is "<<shweta.eId<<endl;
+        cout<<"the value is "<<shweta.favChar<<endl;
+        cout<<"the value is "<<shweta.salary<<endl<<endl;
+        
+        cout<<"the value in beauty"<<endl;
+        beauty.eId=3;
+        beauty.favChar = 'b';
+        beauty.salary=18000;
+        cout<<"the value is "<<beauty.eId<<endl;
+        cout<<"the value is "<<beauty.favChar<<endl;
+        cout<<"the value is "<<beauty.salary<<endl;
+        
+//structure where we dont need to write full employee we or in place of 
+//employee we can use something else like a sortcut or anything............
+    
+    typedef struct employee
+    {
+        int eId;
+        char favChar;
+        float salary;
+    }ep;
+    int main(){
+        ep shwetank;
+        ep shweta;
+        ep beauty;
+        cout<<"the value in shwetank"<<endl;
+        shwetank.eId=1;
+        shwetank.favChar = 'c';
+        shwetank.salary=120000;
+        cout<<"the value is "<<shwetank.eId<<endl;
+        cout<<"the value is "<<shwetank.favChar<<endl;
+        cout<<"the value is "<<shwetank.salary<<endl<<endl;
+        
+        cout<<"the value in shweta"<<endl;
+        shweta.eId=2;
+        shweta.favChar = 's';
+        shweta.salary=19000;
+        cout<<"the value is "<<shweta.eId<<endl;
+        cout<<"the value is "<<shweta.favChar<<endl;
+        cout<<"the value is "<<shweta.salary<<endl<<endl;
+        
+        cout<<"the value in beauty"<<endl;
+        beauty.eId=3;
+        beauty.favChar = 'b';
+        beauty.salary=18000;
+        cout<<"the value is "<<beauty.eId<<endl;
+        cout<<"the value is "<<beauty.favChar<<endl;
+        cout<<"the value is "<<beauty.salary<<endl;
+        
+//union
+    typedef struct employee
+    {
+        int eId;
+        char favChar;
+        float salary;
+    }ep;
+    
+    union money
+    {
+        int price;
+        char car;
+        float pounds;
+    };
+    int main(){
+        ep shwetank;
+        union money m1;
+        m1.price = 34;
+        m1.car='a';
+        cout<<m1.price;
+        
+//enum
+    union money
+    {
+        int price;
+        char car;
+        float pounds;
+    };
+    int main(){
+        enum meal{ breakfast, lunch, dinner };
+        cout<<breakfast<<endl;
+        cout<<lunch<<endl;
+        cout<<dinner<<endl;
+        
+//enum with using the m1 diffrent method
+    union money
+    {
+        int price;
+        char car;
+        float pounds;
+    };
+    int main(){
+        enum meal{ breakfast, lunch, dinner };
+        meal m1 = dinner;
+        cout<<m1;
+        
+//enum some more example to get the concept clearly
+    enum week{sun, mon, tues, wed, thur, fri, sat};
+    int main()
+    {
+        week today;
+        today = wed;
+        cout<<"Day "<<today+1;
+        
+//return value function key
+    int sum(int a, int b){
+        int c = a+b;
+        return c;
+    }
+    int main(){
+        int num1, num2;
+        cout<<"Enter the first number: "<<endl;
+        cin>>num1;
+        cout<<"Enter the second number: "<<endl;
+        cin>>num2;
+        cout<<"sum is: "<<sum(num1, num2);
+        return 0;
+    }
+    
+//function prototype and will tell you what is acceptable or what not acceptable
+    //this is the function prototype
+    int sum(int a,int b); //acceptable
+    int sum(int a, b); //not acceptable
+    int sum(int, int); //acceptable
+    
+    int main(){
+        int num1, num2;
+        cout<<"Enter the first number: "<<endl;
+        cin>>num1;
+        cout<<"Enter the second number: "<<endl;
+        cin>>num2;
+        //this is the actual parameters num1 and num2
+        cout<<"sum is: "<<sum(num1, num2);
+        return 0;
+    }
+    int sum(int a, int b){
+        //this the formal parameters a and b, they will be taking values from the actual parameters that is sum1 and sum2
+        int c = a+b;
+        return c;
+    }
+    */
+//
