53a311fe77014ac3940cc7a7a4dfcd1ce73219ff
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-22 23:09:58 +0530

    Add files via upload

---
+++ Day 7.cpp
@@ -0,0 +1,129 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+//OOPs concept read function, check binary, ones compliment displaying the numbers
+
+/*
+class shwetank
+{
+    string s;
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+void shwetank :: read(void){
+    cout<<"Enter the binary number"<<endl;
+    cin>>s;
+}
+void shwetank :: chk_bin(void){
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if(s.at(i)!='0' && s.at(i)!='1')
+        {
+            cout<<"Inccorect binary"<<endl;
+            exit(0);
+        }
+    }
+}
+
+void shwetank :: ones_compliment(void)
+{
+    for (int i = 0; i < s.length(); i++) {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void shwetank :: display(void)
+{
+    cout<<"Displaying your binary number"<<endl;
+    for (int i = 0; i < s.length(); i++) {
+        cout<<s.at(i);
+    }
+    cout<<endl;
+}
+
+int main(){
+    shwetank b;
+    b.read();
+    b.chk_bin();
+    b.ones_compliment();
+    b.display();
+
+//nesting of member function in c++ on the same above Code
+class shwetank
+{
+private:
+    string s;
+    void chk_bin(void);
+public:
+    void read(void);
+    void ones_compliment(void);
+    void display(void);
+};
+void shwetank :: read(void){
+    cout<<"Enter the binary number"<<endl;
+    cin>>s;
+}
+void shwetank :: chk_bin(void){
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if(s.at(i)!='0' && s.at(i)!='1')
+        {
+            cout<<"Inccorect binary"<<endl;
+            exit(0);
+        }
+    }
+}
+
+void shwetank :: ones_compliment(void)
+{
+    chk_bin(); //nesting function 
+    //A member function can be called by using its name inside 
+    //another member function of the same class. This is known as nesting of member functions.
+    for (int i = 0; i < s.length(); i++) {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void shwetank :: display(void)
+{
+    cout<<"Displaying your binary number"<<endl;
+    for (int i = 0; i < s.length(); i++) {
+        cout<<s.at(i);
+    }
+    cout<<endl;
+}
+
+int main(){
+    shwetank b;
+    b.read();
+    // b.chk_bin();
+    b.ones_compliment();
+    b.display();
+*/
+    return 0;
+}
