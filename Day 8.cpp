a557394208d2c195bae3950c4becd42c19e5f98e
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-23 23:32:15 +0530

    Day 8.cpp

--- Day 8.cpp
+++ Day 8.cpp
@@ -43,3 +43,62 @@ int main(){
     dukaan.displayPrice();
     return 0;
 }
+
+
+
+//.................................................*..................................................
+//day8 of 100 day challenge 
+//#geek for geek code of the day 23-12-2023
+//Count More than n/k Occurences
+    //code in c+++
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        // Your code here
+        unordered_map<int, int>mp;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            mp[arr[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second>n/k) ans++;
+        }
+        return ans;
+    }
+};
+
+//#leetcode code of the day 23-12-2023
+//1496. Path Crossing
+//code in c+++
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        set <int> seen;
+        seen.insert(0);
+        int x=0;
+        int y=0;
+        for(const char c:path){
+            switch(c){
+                case 'N':
+                ++y;
+                break;
+                case 'S':
+                --y;
+                break;
+                case 'E':
+                ++x;
+                break;
+                case 'W':
+                --x;
+                break;
+            }
+            const int key = x * 20001+y;
+            if(seen.count(key))
+                return true;
+            seen.insert (key);
+        }
+        return false;
+    }
+};
