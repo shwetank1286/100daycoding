1246bae1fdcd8fc18449123a0d172c6f12075597
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-24 23:04:33 +0530

    Add files via upload

---
+++ Day 9.cpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+//static data function
+/*
+class Employee{
+    int id;
+
+    public:
+        void setData(void){
+            cout<<"Enter the id"<<endl;
+            cin>>id;
+            count++;
+        }
+        void getData(void){
+            cout<<"the id of this employee is "<<id<<" and this is Employee number "<<count<<endl;
+        }
+};
+
+int Employee ::count;
+
+int main()
+{
+    Employee shwetank, shweta, beauty;
+    shwetank.setData();
+    shwetank.getData();
+    
+    shweta.setData();
+    shweta.getData();
+    
+    beauty.setData();
+    beauty.getData();
+    
+//static member function
+class Employee{
+    int id;
+    static int count;
+    // int count;
+    public:
+        void setData(void){
+            cout<<"Enter the id"<<endl;
+            cin>>id;
+            count++;
+        }
+        void getData(void){
+            cout<<"the id of this employee is "<<id<<" and this is Employee number "<<count<<endl;
+        }
+        static void getCount(void){
+            cout<<"the value of count is "<<count<<endl;
+
+};
+
+int Employee ::count;
+
+int main()
+{
+    Employee shwetank, shweta, beauty;
+    shwetank.setData();
+    shwetank.getData();
+    shwetank.getCount();
+    
+    shweta.setData();
+    shweta.getData();
+    shweta.getCount();
+    
+    beauty.setData();
+    beauty.getData();
+    beauty.getCount();
+*/
+
+    return 0;
+}
