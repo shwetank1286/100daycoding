08b923bd8d624eae92323150ead3540ad79085fe
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-25 22:28:50 +0530

    Day 10.cpp

--- Day 10.cpp
+++ Day 10.cpp
@@ -9,7 +9,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <iostream>
 
 using namespace std;
-/*
+
 //array of object
 class Employee{
     int id;
@@ -91,7 +91,75 @@ int main(){
         c1.setdata(i, j);
         c1.printNumber();
     }
-*/
+
+    
 
     return 0;
 }
+
+//.................................................*..................................................
+//day 10 of 100 day challenge 
+//#geek for geek code of the day 25-12-2023
+//determinant of a merix
+    //code in c+++
+class Solution
+{   
+    public:
+    //Function for finding determinant of matrix.
+   int determinantOfMatrix(vector<vector<int> > matrix, int n)
+    {
+        if(n==1)return matrix[0][0];
+        int m = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            vector<vector<int>>InnerMat(n-1, vector<int>(n-1));
+            int p = 0;
+            for(int j=1; j<n; j++){
+                int q = 0;
+                for(int k=0; k<n; k++){
+                    if(k==i){
+                        continue;
+                    }
+                    InnerMat[p][q++] = matrix[j][k];
+                }
+                p++;
+            }
+            if(i%2==0)ans+=(matrix[m][i])*(determinantOfMatrix(InnerMat,n-1));
+            else ans-=(matrix[m][i])*(determinantOfMatrix(InnerMat,n-1));
+        }
+        return ans;
+    } 
+};
+
+//#leetcode code of the day 25-12-2023
+//decode Ways
+//code in c+++
+class Solution {
+ public:
+  int numDecodings(string s) {
+    const int n = s.length();
+    // dp[i] := the number of ways to decode s[i..n)
+    vector<int> dp(n + 1);
+    dp[n] = 1;  // ""
+    dp[n - 1] = isValid(s[n - 1]);
+
+    for (int i = n - 2; i >= 0; --i) {
+      if (isValid(s[i]))
+        dp[i] += dp[i + 1];
+      if (isValid(s[i], s[i + 1]))
+        dp[i] += dp[i + 2];
+    }
+
+    return dp[0];
+  }
+
+ private:
+  bool isValid(char c) {
+    return c != '0';
+  }
+
+  bool isValid(char c1, char c2) {
+    return c1 == '1' || c1 == '2' && c2 < '7';
+  }
+};
+
