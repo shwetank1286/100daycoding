9ea1a762130f30ca41a34a8424d199d7e9bb51de
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-25 22:26:20 +0530

    Add files via upload

---
+++ Day 10.cpp
@@ -0,0 +1,97 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+/*
+//array of object
+class Employee{
+    int id;
+    int salary;
+    public:
+        void setId(void){
+            salary=122;
+            cout<<"Enter the id of the Employee"<<endl;
+            cin>>id;
+        }
+        void getId(void){
+            cout<<"the id of the Employee is "<<id<<endl;
+        }
+};
+int main()
+{
+    Employee fb[4];
+    for (int i = 0; i < 4; i++) {
+        fb[i].setId();
+        fb[i].getId();
+    }
+    
+//passing object as function
+class complex{
+    int a;
+    int b;
+    public:
+    void setdata(int v1, int v2){
+        a = v1;
+        b = v2;
+    }
+    void setDataBySum(complex o1, complex o2){
+        a=o1.a+o2.a;
+        b=o1.b+o2.b;
+    }
+    void printNumber(){
+        cout<<"your complex number "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+int main(){
+    complex c1, c2, c3;
+    c1.setdata(1, 2);
+    c1.printNumber();
+    
+    c2.setdata(3, 4);
+    c2.printNumber();
+    
+    c3.setdata(6, 5);
+    c3.printNumber();
+
+// passing object as function with for loop
+class complex{
+    int a;
+    int b;
+    public:
+    void setdata(int v1, int v2){
+        a = v1;
+        b = v2;
+    }
+    void setDataBySum(complex o1, complex o2){
+        a=o1.a+o2.a;
+        b=o1.b+o2.b;
+    }
+    void printNumber(){
+        cout<<"your complex number "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+int main(){
+    complex c1;
+    // c1.setdata(1, 2);
+    // c1.printNumber();
+    
+    // c1.setdata(3, 4);
+    // c1.printNumber();
+    
+    // c1.setdata(6, 5);
+    // c1.printNumber();
+    for (int i = 2, j = 4; i < 6, j <8; i++, j++) {
+        c1.setdata(i, j);
+        c1.printNumber();
+    }
+*/
+
+    return 0;
+}
