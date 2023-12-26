e81bb55322190cf65433e15de517bc5c98d34ad8
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-26 23:47:19 +0530

    Day 11.cpp

--- Day 11.cpp
+++ Day 11.cpp
@@ -9,7 +9,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <iostream>
 
 using namespace std;
-/*    
+    
 class complex{
     int a, b;
     public:
@@ -131,7 +131,110 @@ void objects(shwetank & obj)
 int main(){
     shwetank object1;
     objects(object1);
-*/
-
+    
     return 0;
 }
+
+//.................................................*..................................................
+//day 11 of 100 day challenge 
+//#geek for geek code of the day 26-12-2023
+//Largest rectangular sub-matrix whose sum is 0
+    //code in c+++
+class Solution{
+  public:
+  vector<vector<int>> sumZeroMatrix(vector<vector<int>> a){
+      //Code Here
+      int n=a.size();
+      int m=a[0].size();
+      vector<vector<int>> prefix(n+1,vector<int> (m+1,0));
+      pair<int,int> ans1={-1,-1},ans2;
+      int val=-1;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            prefix[i+1][j+1]=prefix[i+1][j]+prefix[i][j+1]+a[i][j]-prefix[i][j];
+            if(prefix[i+1][j+1]==0&&val==-1){
+                ans2={i+1,j+1};
+                ans1={0,0};
+                val=(i+1)*(j+1);
+            }
+            if(prefix[i+1][j+1]==0&&val<((i+1)*(j+1))){
+                ans2={i+1,j+1};
+                ans1={0,0};
+                val=(i+1)*(j+1);
+            }
+        }   
+      }
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              for(int k=0;k<=i;k++){
+                for(int z=0;z<=j;z++){
+                    if(k==i&&z==j) continue;
+                    int ne= prefix[i][j]-prefix[i][z]-prefix[k][j]+ prefix[k][z];
+                    
+                    if(ne==0){
+                        int l=j-z;
+                        int b=i-k;
+                        if(k+1>i||z+1>j)
+                        continue;
+                        if(l*b>val){
+                            ans1={k,z};
+                            ans2={i,j};
+                            val=l*b;
+                        }
+                        if(l*b==val){
+                            if(ans1.second==z){
+                                if((ans2.first-ans1.first)<(i-(k))){
+                                     ans1={k,z};
+                                     ans2={i,j};
+                                     val=l*b;
+                                }
+                            }
+                            else{
+                                if(ans1.second>z){
+                                     ans1={k,z};
+                                     ans2={i,j};
+                                     val=l*b;
+                                }
+                            }
+                        }
+                    }
+                }
+              }
+          }
+      }
+      vector<vector<int>> ans;
+     for(int i=ans1.first;i<ans2.first;i++){
+          vector<int> temp;
+          for(int j=ans1.second;j<ans2.second;j++){
+              temp.push_back(a[i][j]);
+          }
+          ans.push_back(temp);
+      }
+      return ans;
+  }
+};
+
+//#leetcode code of the day 26-12-2023
+//1155. Number of Dice Rolls With Target Sum
+//code in c+++
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n + 1, vector<int> (k * n + 1, 0));
+
+        if(target > k * n)
+            return 0;
+        dp[n][target] = 1;
+
+        const int mod = 1e9 + 7;
+
+        for(int i = n-1; i>-1;i--){
+            for(int j=0; j<k*i+1; j++){
+                for(int z = 1; z<k+1; z++){
+                    dp[i][j]=(dp[i][j] + dp[i+1][j+z]) % mod;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
