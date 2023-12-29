d0f3814e22e12843d36634940e1e8508769d8fbd
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-29 22:55:14 +0530

    Add files via upload

---
+++ Day 14.cpp
@@ -0,0 +1,94 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+/*
+//constructors and distructors
+class complex{
+    int a, b;
+    public:
+    //creating a constructor
+    //construsctor is a special memeber fuction with the same name as of the class.
+    //it is used to initialize the object of its class
+    //it is automatically involved whenever an object is created
+    
+    complex(void);
+    
+    void printNumber(){
+        cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+
+complex :: complex(void){ //this is the defaul constructor
+                          //as it takes no parameters
+    a=10;
+    b=0;
+}
+
+int main()
+{
+    complex c;
+    c.printNumber();
+
+
+//distructors
+//Destructor is an instance member function that is 
+//invoked automatically whenever an object is going to be destroyed.
+class complex{
+    int a, b;
+public:
+    
+    complex(void);
+    
+    void printNumber(){
+        cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+
+complex :: complex(void)
+{
+    a = 0;
+    b = 0;
+}
+
+int main(){
+    complex a;
+    a.printNumber();
+  
+//paramaterized function
+class complex{
+    int a, b;
+public:
+    
+    complex(int, int);
+    
+    void printNumber(){
+        cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+
+complex :: complex(int x, int y)
+{
+    a = x;
+    b = y;
+}
+
+int main(){
+    //implicit call
+    complex a(4, 6);
+    //explicit call
+    complex b= complex(5, 7);
+    
+    a.printNumber();
+    b.printNumber();
+*/
+
+    return 0;
+}
