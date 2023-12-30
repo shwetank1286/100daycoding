cf6fa86f6a2940a32f9c96cb9b3914ebbfcd9a27
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-30 23:55:14 +0530

    Add files via upload

---
+++ Day 15.cpp
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+//prameterised consytructors
+//point constructor continue
+/*
+class point{
+    int x, y;
+    public:
+        point(int a, int b){
+            x = a;
+            y = b;
+        }
+        void displaypoint(){
+            cout<<"the point is "<<"("<<x<<","<<y<<")"<<endl;
+        }
+        
+};
+
+int main()
+{
+    point p(1,1);
+    p.displaypoint();
+    point q(2,2);
+    q.displaypoint();
+
+//example of pareameterised constructor 
+//create a function (hint: make it as a friend) which take twop point object and computes
+//the distance between 2 points
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+class point{
+    int x ,y ;
+    friend void distance(point o1 ,point o2 );
+    public:
+    point (int a ,int b){
+        x=a;
+        y=b;
+    }
+    void display(){
+        cout<<"("<<x<<","<<y<<")"<<endl;
+    }
+};
+void distance(point o1 ,point o2){
+    float dist;
+dist=sqrt(pow(o2.x-o1.x ,2)+pow(o2.y-o1.y ,2));
+cout<<dist<<endl;
+}
+int main()
+{
+   point p(1,1);
+   p.display();
+   point q(2,2);
+   q.display(); 
+   distance(p,q);
+*/
+    return 0;
+}
